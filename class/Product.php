2d9e6a2d35cacdace7edeb43de869114dec3b357
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-26 18:35:24 +0200

    [UPD] start ereditarietà

--- class/Product.php
+++ class/Product.php
@@ -1,8 +1,41 @@
 <?php
 class Product
 {
-    private int $id;
-    private string $food;
-    private  string $play;
-    private string $bed;
+    protected int $id;
+
+    //costruttore
+    public function __construct(int $_id)
+    {
+        //cosi facendo assegnamo dei parametri $_ alle nostre istanze
+        $this->id = $_id;
+    }
+    /*************/
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+    /*************/
 }
+
+// CLASSI FIGLI 
+//Cat
+class Cat extends Product
+{
+}
+//Dog
+class Dog extends Product
+{
+}
+
+//FUORI DALLE CLASSI
+$product = new Product(1);
+$Cat = new Product(2);
+$Dog = new Product(3);
+var_dump($product);
+var_dump($Cat);
+var_dump($Dog);
